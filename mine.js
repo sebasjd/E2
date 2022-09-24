13726ce715b6ff30d0c7ac117fe2427fafc84ece
bdiegoa <d.jsebastian@hotmail.com>
2022-09-23 22:35:03 -0300

    E3

--- mine.js
+++ mine.js
@@ -2,24 +2,27 @@ const input = document.getElementById("input");
 const h2 = document.getElementById("h2");
 const h4 = document.getElementById("h4");
 const btn = document.getElementById("btn");
+const imgg = document.querySelector("#imgg");
+const form = document.querySelector("form");
 
 
 class pizza {
-    constructor(ID, nombre, ingredientes, precio) {
+    constructor(ID, nombre, ingredientes, precio, img) {
         this.ID = ID
         this.nombre = nombre
         this.ingredientes = ingredientes
         this.precio = precio
+        this.img = img
     }
 
 }
 
-let pizzas = [muzzarella = new pizza(`1`, `Muzzarella`, ingredientes = [`tomate`, `muzzarella`, `oregano`], 800),
-    especial = new pizza(`2`, `Especial`, ingredientes = [`tomate`, `muzzarella`, `jamon`, `huevo`], 1000),
-    cuatroQuesos = new pizza(`3`, `Cuatro Quesos`, ingredientes = [`tomate`, `muzzarella`, `parmesano`, `probolone`, `barra`], 1200),
-    palmitos = new pizza(`4`, `Palmitos`, ingredientes = [`tomate`, `muzzarella`, `palmitos`, `salas golf`], 1300),
-    anana = new pizza(`5`, `Anana`, ingredientes = [`tomate`, `muzzarella`, `anana`, `azucar morena`], 1500),
-    sinQueso = new pizza(`6`, `Sin Queso`, ingredientes = [`tomate`, `oregano`], 500),
+let pizzas = [muzzarella = new pizza(`1`, `Muzzarella`, ingredientes = [`tomate`, `muzzarella`, `oregano`], 800, `muzza.jpg`),
+    especial = new pizza(`2`, `Especial`, ingredientes = [`tomate`, `muzzarella`, `jamon`, `huevo`], 1000, `espe.jfif`),
+    cuatroQuesos = new pizza(`3`, `Cuatro Quesos`, ingredientes = [`tomate`, `muzzarella`, `parmesano`, `probolone`, `barra`], 1200, `4q.png`),
+    palmitos = new pizza(`4`, `Palmitos`, ingredientes = [`tomate`, `muzzarella`, `palmitos`, `salas golf`], 1300, `palmitos.jpg`),
+    anana = new pizza(`5`, `Anana`, ingredientes = [`tomate`, `muzzarella`, `anana`, `azucar morena`], 1500, `anana.jpg`),
+    sinQueso = new pizza(`6`, `Sin Queso`, ingredientes = [`tomate`, `oregano`], 500, `sinq.jpg`),
 ]
 
 // guardo el array en el localStorage
@@ -31,36 +34,40 @@ saveLocalStorage(pizzas)
 // Recupero el array del localStorage
 let getPizzas = JSON.parse(localStorage.getItem('pizzas2'));
 
-/////////////////////////////////
-//    El localStorage solo se usó para practicar, no es necesario
-/////////////////////////////////
 
 
-// Función que se ejecuta si el resultado es correcto
-function filtro(inputID) {
+
+//  renderizar pizza
+function renderPizza(inputID) {
     const laPizza = pizzas.filter((piza) => piza.ID == inputID)
     for (piza of laPizza) {
         h2.innerHTML = `${piza.nombre}`;
         h4.innerHTML = `$ ${piza.precio}`;
+        h42.innerHTML = `${piza.ingredientes.join(", ")}`;
+        imgg.setAttribute("src", `./images/${piza.img}`);
     }
 }
 
 // Función que se ejecuta si el resultado es erroneo
-function error() {
+function error(mesage) {
 
-    h2.innerHTML = `Sin stock`;
+    h2.innerHTML = mesage;
     h4.innerHTML = ``;
+    h42.innerHTML = ``;
+    imgg.setAttribute("src", `./images/error.png`);
 
 }
+
+
 
 
 // acción del boton
 btn.addEventListener('click', button)
 
-
 function button(e) {
     e.preventDefault();
     var inputID = input.value.trim();
-    inputID <= pizzas.length && inputID > 0 ? filtro(inputID) : error();
+    inputID <= pizzas.length && inputID > 0 ? renderPizza(inputID) : inputID == "" ? error("Debe ingresar un ID") : error("ID invalido");
     input.value = ""
+
 }
